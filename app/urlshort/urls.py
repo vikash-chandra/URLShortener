0b0dd6f183bacb69e3ad71b4bf05ff7dfb6a5baf
vikash-chandra <vikashchandra11@gmail.com>
2021-08-01 16:11:18 +0530

    URL shortening and redirction API created

--- app/urlshort/urls.py
+++ app/urlshort/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from urlshort.views import home
+from urlshort.views import home, createShortURL, redirect
 
 urlpatterns = [
     path('', home),
+    path('create/', createShortURL, name='create'),
+    path('<str:url>', redirect, name='redirect'),
 ]
--- app/urlshort/views.py
+++ app/urlshort/views.py
@@ -1,6 +1,63 @@
 from django.shortcuts import render
+from .models import ShortURL
+from .forms import CreateNewShortURL
+from datetime import datetime
+import random, string
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def createShortURL(request):
+    '''
+    create short url if 
+        request type :
+            POST: if already exists given url
+                  return already exist short url
+            other:
+                   return a form context
+    '''
+    
+    if request.method == 'POST':
+        form = CreateNewShortURL(request.POST)
+        if form.is_valid():
+            original_website = form.cleaned_data['original_url']
+            original_website_exists = ShortURL.objects.filter(original_url=original_website)
+            if len(original_website_exists)>0:
+                print("Already created...")
+                return render(request, 'urlcreated.html', {'url_code': original_website_exists[0].short_url})
+
+            random_chars = generate_MD5_hash()
+            while len(ShortURL.objects.filter(short_url=random_chars)) !=0:
+                random_chars = generate_MD5_hash()
+            s = ShortURL(original_url=original_website, short_url=random_chars, time_date_created=datetime.now())
+            s.save()
+        return render(request, 'urlcreated.html', {'url_code': s.short_url})
+    form = CreateNewShortURL()
+    context = {'form': form}
+    return render(request, 'create.html', context)
+
+def redirect(request, url):
+    '''
+    Redirect to given url if short url created
+    '''
+    # Later if want to change in hash generation
+    # Modify this function only
+
+    current_url = ShortURL.objects.filter(short_url=url)
+    if len(current_url) <=0:
+        return render(request, 'pagenotfound.html')
+    context = {'obj':current_url[0]}
+    return render(request, 'redirect.html', context)
+
+def generate_hash():
+    '''
+    Genreate 6 character hash code
+    '''
+
+    random_char_lst = list(string.ascii_letters + string.digits)
+    random_chars = ''
+    for i in range(6):
+        random_chars += random.choice(random_char_lst)
+    return random_chars
